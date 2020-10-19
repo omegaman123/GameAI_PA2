0fc9a01f7836ab65276c702d36d78ecb481c232e
Mark <35475840+omegaman123@users.noreply.github.com>
2020-10-19 15:28:41 -0700

    add map

--- src/p2_interactive.py
+++ src/p2_interactive.py
@@ -78,12 +78,12 @@ def on_click(event):
     else:
         destination_point = event.y*SUBSAMPLE, event.x*SUBSAMPLE
         try:
-            # path, visited_boxes = p2_pathfinder.find_path(source_point, destination_point, mesh)
+            path, visited_boxes = p2_pathfinder.find_path(source_point, destination_point, mesh)
 
             # path, visited_boxes = p2_pathfinder.find_path((314, 608), (278, 518), mesh)
 
             # path, visited_boxes = p2_pathfinder.find_path( (468, 398), (586, 320), mesh)
-            path, visited_boxes = p2_pathfinder.find_path((584, 650), (322, 354), mesh)
+            # path, visited_boxes = p2_pathfinder.find_path((584, 650), (322, 354), mesh)
             # path, visited_boxes = p2_pathfinder.find_path((636, 120), (224, 232), mesh)
 
             # path, visited_boxes = p2_pathfinder.find_path((328, 368), (474, 386), mesh)
